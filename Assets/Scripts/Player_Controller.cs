f4b377d242e7ab7aacc181eefcc4b23f533a75e6
Iancic <davidiancic05@gmail.com>
2022-10-22 00:44:44 +0300

    Implementarea sistemului de launch cu elan.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float thrust;
-    public int charges = 300;
+    public float thrustlow, thrustmid, thrusthigh, spedometer;
+    private int i, launched = 0;
     void Start()
     {
+        StartCoroutine(Launch());
         rb = GetComponent<Rigidbody2D>();
     }
-
     
-    void Update()
+    IEnumerator Launch()
     {
-         if (Input.GetKeyDown(KeyCode.Q) && charges > 0)
-            {
-                rb.AddForce(new Vector2 (0f, thrust));
-                charges = charges -1;
-
-            }
+        launched = 0;
+        do
+        {
+        for (i=1; i<100; i++){
+            Debug.Log(i);
+            if (Input.GetKey(KeyCode.Q)){
+                launched = 1;
+                if (i<=25)
+                rb.AddForce(new Vector2 (0f, thrustlow));
+                else if (i>25 && i <=75)
+                rb.AddForce(new Vector2 (0f, thrustmid));
+                else if (i>75)
+                rb.AddForce(new Vector2 (0f, thrusthigh));
+                break;}
+            yield return new WaitForSeconds(spedometer);}
+        if (launched == 0) // if adaugat deorece la primul click iese din for dar al doilea nu este breaked
+        {
+            for (i=100; i>0; i--){
+            Debug.Log(i);
+            if (Input.GetKey(KeyCode.Q)){
+                Debug.Log("MATA");
+                launched = 1;
+                if (i<=25)
+                rb.AddForce(new Vector2 (0f, thrustlow));
+                else if (i>25 && i <=75)
+                rb.AddForce(new Vector2 (0f, thrustmid));
+                else if (i>75)
+                rb.AddForce(new Vector2 (0f, thrusthigh));
+                break;}
+            yield return new WaitForSeconds(spedometer);}
+        }
+        } while ( launched == 0);
+        
     }
 }
